3bb228ae10428a085f185a170a500799527b126a
Kacper Rączy <gfw.kra@gmail.com>
2021-11-17 14:55:15 +0100

    Added option to store multiple build strings in custom field

--- config/build.go
+++ config/build.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"fmt"
+	"strings"
 )
 
 type Build struct {
@@ -11,4 +12,19 @@ type Build struct {
 
 func (build *Build) String() string {
 	return fmt.Sprintf("%v (%v)", build.Version, build.Number)
+}
+
+func ParseBuild(s string) (*Build, error) {
+	components := strings.Split(s, " ")
+	if len(components) != 2 {
+		return nil, fmt.Errorf("Provided string does not have required components")
+	}
+
+	version := components[0]
+	number := strings.Trim(components[1], "() ")
+	build := new(Build)
+	build.Version = version
+	build.Number = number
+
+	return build, nil
 }
--- service/jira.go
+++ service/jira.go
@@ -2,12 +2,17 @@ package service
 
 import (
 	"fmt"
+	"log"
+	"sort"
+	"strings"
 
 	"github.com/Holdapp/bitrise-step-jira-build/config"
 	logger "github.com/bitrise-io/go-utils/log"
 
 	"github.com/andygrunwald/go-jira"
 )
+
+const MultiFieldSeparator string = ", "
 
 type JIRAWorker struct {
 	Auth          jira.BasicAuthTransport
@@ -38,19 +43,69 @@ func NewJIRAWorker(baseURL string, username string, password string, customField
 func (worker *JIRAWorker) UpdateBuildForIssues(issueKeys []string, build config.Build) {
 	for _, key := range issueKeys {
 		buildString := build.String()
-		customFieldKey := fmt.Sprintf("customfield_%v", worker.CustomFieldID)
 
-		fields := map[string]string{
-			customFieldKey: buildString,
-		}
-		body := map[string]interface{}{
-			"fields": fields,
-		}
+		log.Printf("New custom field content: \"%v\" \n", buildString)
 
-		_, err := worker.Client.Issue.UpdateIssue(key, body)
+		err := worker.sendUpdateCustomFieldRequest(key, buildString)
 		if err != nil {
-			logger.Warnf("Error for '%s': %v\n", key, err)
-			// TODO Response body
+			logger.Warnf("Error while updating '%s': %v\n", key, err)
 		}
 	}
+}
+
+func (worker *JIRAWorker) UpdateBuildForIssuesMultiField(issueKeys []string, build config.Build) {
+	for _, key := range issueKeys {
+		fields, _, err := worker.Client.Issue.GetCustomFields(key)
+		if err != nil {
+			logger.Warnf("Error while getting custom fields of %s: %v\n", key, err)
+			continue
+		}
+
+		// parse existing builds
+		customFieldKey := fmt.Sprintf("customfield_%v", worker.CustomFieldID)
+		currentFieldContent := fields[customFieldKey]
+		currentBuildStrings := strings.Split(currentFieldContent, MultiFieldSeparator)
+		currentBuilds := make([]config.Build, 0)
+		for _, s := range currentBuildStrings {
+			build, err := config.ParseBuild(s)
+			if err == nil {
+				currentBuilds = append(currentBuilds, *build)
+			}
+		}
+
+		// create new build string
+		newBuildStrings := make([]string, 0)
+		newBuildStrings = append(newBuildStrings, build.String())
+		for _, currentBuild := range currentBuilds {
+			if currentBuild.Version != build.Version {
+				newBuildStrings = append(newBuildStrings, currentBuild.String())
+			}
+		}
+		sort.Strings(newBuildStrings)
+		newFieldContent := strings.Join(newBuildStrings, MultiFieldSeparator)
+
+		log.Printf("Current custom field content: \"%v\" \n", currentFieldContent)
+		log.Printf("Current build list: %v , len: %d\n", currentBuildStrings, len(currentBuildStrings))
+		log.Printf("New build list: %v , len: %d\n", newBuildStrings, len(newBuildStrings))
+		log.Printf("New custom field content: \"%v\" \n", newFieldContent)
+
+		err = worker.sendUpdateCustomFieldRequest(key, newFieldContent)
+		if err != nil {
+			logger.Warnf("Error while updating '%s': %v\n", key, err)
+		}
+	}
+}
+
+func (worker *JIRAWorker) sendUpdateCustomFieldRequest(issueKey string, buildString string) error {
+	customFieldKey := fmt.Sprintf("customfield_%v", worker.CustomFieldID)
+	fields := map[string]string{
+		customFieldKey: buildString,
+	}
+	body := map[string]interface{}{
+		"fields": fields,
+	}
+
+	_, err := worker.Client.Issue.UpdateIssue(issueKey, body)
+
+	return err
 }
